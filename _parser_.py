d49341afbc497d038a8f2204cf5e4197098ce7d3
mercarto95 <ahmeedo.khaled@outlook.com>
2023-01-08 22:55:46 +0100

    under development

--- _parser_.py
+++ _parser_.py
@@ -1,6 +1,6 @@
 import os
 
-f = open("./data/data.bi", "rb")
+f = open("../data/test.bi", "rb")
 
 file = f.read()
 if len(file) == 0:
@@ -34,6 +34,8 @@ def handle_country(param):
         return False
     return True
 
+
+
 def push_the_line(country, info):
     for index in memory:
         if index[0] == country:
@@ -44,6 +46,51 @@ def push_the_line(country, info):
     memory.append(tmp)
     return False
 
+channels_list = []
+movies_list = []
+series_list = []
+
+
+def push_to_channel(country, info):
+    for index in channels_list:
+        if index[0] == country:
+            index[1].append(info)
+            return True
+    # then new category, push it to the memory
+    tmp = [country, [info]]
+    channels_list.append(tmp)
+    return True
+
+def push_to_series(country, info):
+    for index in series_list:
+        if index[0] == country:
+            index[1].append(info)
+            return True
+    # then new category, push it to the memory
+    tmp = [country, [info]]
+    series_list.append(tmp)
+    return True
+
+def push_to_movie(country, info):
+    for index in movies_list:
+        if index[0] == country:
+            index[1].append(info)
+            return True
+    # then new category, push it to the memory
+    tmp = [country, [info]]
+    movies_list.append(tmp)
+    return True
+
+
+def check_type(link):
+    link = link.split("/")
+    if "series" in link:
+        return "series"
+    elif "movie" in link:
+        return "movie"
+    else:
+        return "channel"
+    
 
 countries = []
 tot = []
@@ -59,15 +106,30 @@ for line in lines:
             if handle_country(buffer):
                 #push_the_line(buffer, line)
                 print(buffer)
+                if buffer == '':
+                    buffer = word
                 countries.append(buffer)
             recognized_group_title = False 
+            x = check_type(details[-1])
+            if x == "channel":
+                push_to_channel(buffer, line)
+            elif x == "movie":
+                push_to_movie(buffer, line)
+            elif x == "series":
+                push_to_series(buffer, line)
             push_the_line(buffer, line)
+
         if word == " group-title=":
             #buffer += word
             recognized_group_title = True
 
 
 
+
+
 pass
 
+
+
+## parse movies 
 
--- channel.py
+++ channel.py
@@ -1,6 +1,38 @@
 
-
-
 class Channel:
-    def __init__(self):
-        pass 
+    def __init__(self, line):
+        self.parse_line(line)
+        self.channel_id = self.parse_id(line)
+        self.channel_name = self.parse_name(line)
+        self.logo = self.pase_logo(line)
+        self.group = self.parse_group(line)
+        self.link = self.parse_link(line)
+    
+    def parse_line(self, line):
+        x = line.split('"')
+        return x[7]
+    
+    def parse_logo(self, line):
+        x = line.split('"')
+        return x[5]
+    
+    def parse_name(self, line):
+        x = line.split('"')
+        return x[3]
+    
+    def parse_id(self, line):
+        x = line.split('"')
+        return x[1]
+    
+    def parse_link(self, line):
+        line = line.split(" ")
+        info = line[-1].split("\\")
+        length_list = []
+        for i in info:
+            length_list.append( len(i) )
+        index = length_list.index( max(length_list) )
+        link = info[index]
+        link =info[1:]
+        return link
+
+        
